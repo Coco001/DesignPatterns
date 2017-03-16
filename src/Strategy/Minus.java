ef00df6a5d0390ff470c1cc1729d82e257e99133
Coco001 <wsgkk000@163.com>
2017-03-16 12:27:28 +0800

    策略模式

---
+++ src/Strategy/Minus.java
@@ -0,0 +1,14 @@
+package Strategy;
+
+/**
+ * 实现类
+ */
+public class Minus extends AbstractCalculator implements ICalculator {
+
+    @Override
+    public int calculate(String exp) {
+        int arrayInt[] = split(exp,"-");
+        return arrayInt[0]-arrayInt[1];
+    }
+}
+
---
+++ src/Strategy/Plus.java
@@ -0,0 +1,14 @@
+package Strategy;
+
+/**
+ * 实现类
+ */
+public class Plus extends AbstractCalculator implements ICalculator {
+
+    @Override
+    public int calculate(String exp) {
+        int arrayInt[] = split(exp,"\\+");
+        return arrayInt[0]+arrayInt[1];
+    }
+}
+
