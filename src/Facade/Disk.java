2e71cf48296730ec37181e6c26b51d7c3e35ddd2
Coco001 <wsgkk000@163.com>
2017-03-16 11:27:34 +0800

    外观模式

---
+++ src/Facade/Disk.java
@@ -0,0 +1,15 @@
+package Facade;
+
+/**
+ * 其中一个类
+ */
+public class Disk {
+    public void startup(){
+        System.out.println("disk startup!");
+    }
+
+    public void shutdown(){
+        System.out.println("disk shutdown!");
+    }
+
+}
---
+++ src/Facade/Memory.java
@@ -0,0 +1,15 @@
+package Facade;
+
+/**
+ * 其中一个类
+ */
+public class Memory {
+    public void startup(){
+        System.out.println("memory startup!");
+    }
+
+    public void shutdown(){
+        System.out.println("memory shutdown!");
+    }
+
+}
